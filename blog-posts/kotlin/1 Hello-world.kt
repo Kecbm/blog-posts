e8717062dfab03f0c29ca5a02cd59a7b4a6adbe6
Klecianny Melo <kleciannymelo@gmail.com>
2023-06-26 20:55:41 -0300

    :sparkles: arquivo do Hello world em Kotlin

---
+++ blog-posts/kotlin/1 Hello-world.kt
@@ -0,0 +1,38 @@
+fun main() {
+    println("Hello, world!")
+}
+
+fun main() {
+    println("Kotlin é recomendado")
+    println("para o desenvolvimento")
+    println("de aplicativos Android")
+}
+
+fun main() {
+    println("Meu nome é Klecianny")
+    print("E eu tenho ")
+    print("27 anos.")
+    print(" Atualmente estudo desenvolvimento Android")
+}
+
+fun main() {
+    println("Meu nome é Klecianny")
+    print("\n E eu tenho ")
+    print("\n 27 anos.")
+    print("\n Atualmente estudo desenvolvimento Android")
+}
+
+fun main() {
+    println("A próxima linha está em branco:")
+    println("")
+    println("E aqui termina o conteúdo.")
+}
+
+fun main() {
+    println("Obrigada por ler meu post!")
+
+    //  Imprime uma linha em branco
+    println("")
+
+    println("Até a próxima :D")
+}
